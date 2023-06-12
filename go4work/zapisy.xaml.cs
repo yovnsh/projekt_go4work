4d89a477b7095397fd3e358fbb6b0a757dcdba27
gabriela goliszke <yovnsh@gmail.com>
2023-06-12 13:06:32 +0200

    wyszukiwarka zleceń w hotel? trivago

--- go4work/zapisy.xaml.cs
+++ go4work/zapisy.xaml.cs
@@ -31,6 +31,18 @@ namespace go4work
         {
             InitializeComponent();
 
+            // ręcznie ładujemy hotele za każdym razem
+            // może też wygodniej by było przechowywać je w pamięci?
+            // A PRZEDE WSZYSTKIM W ODDZIELNEJ FUNKCJI
+            string command = $"select id, name from hotels;";
+            SqlCommand sql_command = new SqlCommand(command, App.connection);
+            SqlDataReader reader = sql_command.ExecuteReader();
+            while (reader.Read())
+            {
+                HotelList.Items.Add(new ComboBoxItem() { Tag = reader.GetInt32(0), Content = reader.GetString(1) });
+            }
+            reader.Close();
+
             // jeśli nie ma załadowanych ofert to je załaduj
             if (CachedOffers.Count == 0)
             {
@@ -39,13 +51,21 @@ namespace go4work
             UpdateOfferList(); // wyświetla załadowane oferty
         }
 
-        // obsługuje przycisk "odśwież"
-        private void Refresh(object sender, RoutedEventArgs e)
+        // obsługuje przycisk szukaj
+        private void SearchOffers(object sender, RoutedEventArgs e)
         {
             LoadOffers();
             UpdateOfferList();
         }
 
+        // czyści filtry wyszukiwania
+        private void ClearFilters(object sender, RoutedEventArgs e)
+        {
+            HotelList.SelectedIndex = 0;
+            DateFilter.SelectedDate = null;
+            LoadOffers();
+            UpdateOfferList();
+        }
 
         // ładuje oferty z bazy danych i akutalizuje listę oraz zmienną CachedOffers
         // może w przyszłości uda się zrobić to asynchronicznie?
@@ -54,14 +74,38 @@ namespace go4work
             // zanim załadujemy nowe oferty wyczyść listę - zaczynamy od zera
             CachedOffers.Clear();
 
-            string command = $"select * from work_offers;";
+            string command = $"select hotels.name as 'hotel_name', date, hours, salary from work_offers left join hotels on work_offers.hotel_id=hotels.id";
+
+            bool search_active = false; // czy where już zostało wcześniej dodane żeby się nie powtarzało
+
+            // jeśli jakiekolwiek wyszukiwanie jest aktywne to dodajemy je do zapytania sql
+            if (HotelList.SelectedItem != null && (HotelList.SelectedItem as ComboBoxItem).Tag.ToString() != "*")
+            {
+                command += $" where hotel_id={Convert.ToInt32((HotelList.SelectedItem as ComboBoxItem).Tag)}";
+                search_active = true; // informujemy że już jest jakiś warunek
+            }
+
+            if (DateFilter.SelectedDate != null)
+            {
+                if (!search_active)
+                {
+                    command += " where ";
+                    search_active = true;
+                }
+                else
+                {
+                    command += " and ";
+                }
+
+                command += $"date='{DateFilter.SelectedDate.Value.ToString("yyyy-MM-dd")}'"; // w takim formacie daty przyjmuje baza danych
+            }
 
             SqlCommand sql_command = new SqlCommand(command, App.connection);
             SqlDataReader reader = sql_command.ExecuteReader();
 
             while (reader.Read())
             {
-                // wiersz bazy danych wygląda tak tak: hotel_name, date, hours, salary
+                // wiersz bazy danych wygląda tak tak: hotel_name, data, godziny, wynagrodzenie
                 CachedOffers.Add(new work_offer(reader.GetString(0), reader.GetDateTime(1), reader.GetInt32(2), reader.GetInt32(3)));
             }
             reader.Close();
